49785ec2976d771cb1ab76fab39b5d0762522133
gzheng2 <gzheng@eagc.test>
2020-11-30 16:24:27 -0500

    created preproc to host data load scripts

--- scripts/load_l1000.sh
+++ scripts/load_l1000.sh
@@ -1,13 +1,13 @@
 date
 echo load part 1
-python ../src/load_data.py L1000-1.tsv
+python ../src/preproc/load_data.py L1000-1.tsv
 date
 echo load part 2
-python ../src/load_data.py L1000-2.tsv
+python ../src/preproc/load_data.py L1000-2.tsv
 date
 echo load part 3
-python ../src/load_data.py L1000-3.tsv
+python ../src/preproc/load_data.py L1000-3.tsv
 date
 echo load part 4
-python ../src/load_data.py L1000-4.tsv
+python ../src/preproc/load_data.py L1000-4.tsv
 date
