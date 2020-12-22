9b45cbc0e8cc9f009a914ef9912298a8d949f349
Yi-Pei Chen <52116103+bbchen33@users.noreply.github.com>
2020-12-22 11:07:56 -0700

    updated R codes to filter expression file to only expression data in the network

--- rscripts/wgcna_codes.R
+++ rscripts/wgcna_codes.R
@@ -4,15 +4,24 @@ install.packages("BiocManager")
 BiocManager::install("WGCNA")
 library(WGCNA)
 
+#Load expression df with normalized count and the network ID file
+expression = read.table('./Data/kapoor2019_batch.age.rin.sex.pm.alc.corrected.coga.inia.expression.txt',
+                        header = TRUE)
+network_IDs = read.csv('./Data/eda_derived/network_IDs.csv',
+                       row.names = 1)
+## Filter expression for network only expression
+network_only_expression = expression[expression$id %in% network_IDs$X0,]
+rownames(network_only_expression) = NULL
+write.csv(network_only_expression, 
+          './Data/eda_derived/network_only_expression.csv',
+          row.names = F)
+
 ## automatic block-wise network
-# load expression data
-expression = read.csv('G:/Shared drives/NIAAA_ASSIST/Data/eda_derived/network_only_expression.csv', 
-                      header = T, row.names = 1)
 # transpose the dataframe
-expression_t = t(expression)
+network_only_expression_t = t(network_only_expression)
 
 # blockwiseModules() will generate Tom network along with the module detection by WCGNA
-net = blockwiseModules(expression_t, power = 14,
+net = blockwiseModules(network_only_expression_t, power = 14,
                        TOMType = "unsigned", minModuleSize = 100,
                        reassignThreshold = 0, detectCutHeight = 0.99,
                        numericLabels = TRUE, pamRespectsDendro = FALSE,
@@ -21,11 +30,9 @@ net = blockwiseModules(expression_t, power = 14,
                        verbose = 3, deepSplit = T)
 
 # summary of the network modules (colors represent module assignment)
-# table(net$colors)
-
 net_df = data.frame(net$colors) # convert to a df
 net_df = cbind(id = rownames(net_df), net_df) # change the index (node names) to a column
 rownames(net_df) = 1:nrow(net_df)
 colnames(net_df)[2] = 'louvain_label' # change column name
 
-write.csv(net_df, './Data/eda_derived/wgcna_modules_test.csv', row.names = F)
+write.csv(net_df, './Data/eda_derived/wgcna_modules.csv', row.names = F)
