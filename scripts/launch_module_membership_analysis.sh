eb8f515a60fd8100329e44da7f95a6626e335554
gzheng2 <gzheng@eagc.test>
2021-01-04 20:15:01 -0500

    added module_membership_analysis and json config files

---
+++ scripts/launch_module_membership_analysis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo ${SCRIPT_DIR}
+
+DATA="/Volumes/GoogleDrive/Shared drives/NIAAA_ASSIST/Data"
+MODULE="module_membership_analysis"
+
+if [ ! -d "${DATA}/${MODULE}" ] 
+then
+    mkdir "${DATA}/${MODULE}"
+fi
+
+cp "${SCRIPT_DIR}/${MODULE}".json "${DATA}/${MODULE}"/config.json
+
+# Takes about 13 minutes to run
+date
+docker run -m16g --rm -e config_file="${MODULE}/config.json" -e archive_path="${MODULE}/run1" -v "${DATA}":/assist/Data assist/${MODULE}:0.1.0
+date
