971c6a1c598568fd73983a9db013d00cbe184199
Yi-Pei Chen <52116103+bbchen33@users.noreply.github.com>
2021-02-03 16:47:02 -0800

    1. Updated R codes to take config path and archive path 2. Updated mouse network_analysis.json

--- analyses/network_analysis/wgcna_codes.R
+++ analyses/network_analysis/wgcna_codes.R
@@ -2,32 +2,22 @@
 
 library(WGCNA)
 library("rjson")
+args = commandArgs(trailingOnly = T)
+config_file = args[1]
+archive_path = args[2]
 
-json_data <- fromJSON(file='./Data/pipeline/human/network_analysis/config.json')
-#Load expression df with normalized count and the network ID file
+json_data = fromJSON(file = config_file)
 expression = read.table(file.path('./Data', json_data['normalized_counts']), header = TRUE)
-network_IDs = read.csv('./Data/pipeline/human/network_analysis/network_IDs.csv', row.names = 1)
+rownames(expression) = expression$id
+expression$id = NULL
+expression_t = t(expression)
 
-## Filter expression for network only expression
-network_only_expression = expression[expression$id %in% network_IDs$X0,]
-
-# Convert the id column to index and delete id column
-rownames(network_only_expression) = network_only_expression$id
-network_only_expression$id = NULL
-
-## automatic block-wise network
-# transpose the dataframe
-network_only_expression_t = t(network_only_expression)
-
-# blockwiseModules() will generate Tom network along with the module detection by WCGNA
-net = blockwiseModules(network_only_expression_t, power = 14,
+net = blockwiseModules(expression_t, power = 14,
                        TOMType = "unsigned", minModuleSize = 100,
                        reassignThreshold = 0, detectCutHeight = 0.99,
                        numericLabels = TRUE, pamRespectsDendro = FALSE,
                        saveTOMs = F,
-                       #   saveTOMFileBase = "femaleMouseTOM",
                        verbose = 3, deepSplit = T)
-
 # summary of the network modules (colors represent module assignment)
 net_df = data.frame(net$colors) # convert to a df
 net_df = cbind(id = rownames(net_df), net_df) # change the index (node names) to a column
@@ -35,4 +25,44 @@ rownames(net_df) = 1:nrow(net_df)
 colnames(net_df)[2] = 'louvain_label' # change column name
 
 # change the file name below to wgcna_modules.csv during test
-write.csv(net_df, './Data/pipeline/human/network_analysis/wgcna_modules.csv', row.names = F)
+# write.csv(net_df, './Data/pipeline/human/network_analysis/wgcna_modules.csv', row.names = F)
+write.csv(net_df, file.path(archive_path, 'wgcna_modules.csv'), row.names = F)
+
+
+
+# json_data <- fromJSON(file='./Data/pipeline/human/network_analysis/config.json')
+#Load expression df with normalized count and the network ID file
+# expression = read.table(file.path('./Data', json_data['normalized_counts']), header = TRUE)
+# network_IDs = read.csv('./Data/pipeline/human/network_analysis/network_IDs.csv', row.names = 1)
+# 
+# expression = read.table('G:/Shared drives/NIAAA_ASSIST/Data/HDID_data/PFC_HDID_norm_exp.txt', header = TRUE)
+# kapoor_expression = read.table('G:/Shared drives/NIAAA_ASSIST/Data/kapoor2019_batch.age.rin.sex.pm.alc.corrected.coga.inia.expression.txt', header = TRUE)
+# ## Filter expression for network only expression
+# # network_only_expression = expression[expression$id %in% network_IDs$X0,]
+# 
+# # Convert the id column to index and delete id column
+# rownames(expression) = expression$probeID
+# expression$probeID = NULL
+# 
+# ## automatic block-wise network
+# # transpose the dataframe
+# expression_t = t(expression)
+# # network_only_expression_t = t(network_only_expression)
+# # blockwiseModules() will generate Tom network along with the module detection by WCGNA
+# net = blockwiseModules(expression_t, power = 14,
+#                        TOMType = "unsigned", minModuleSize = 100,
+#                        reassignThreshold = 0, detectCutHeight = 0.99,
+#                        numericLabels = TRUE, pamRespectsDendro = FALSE,
+#                        saveTOMs = F,
+#                        #   saveTOMFileBase = "femaleMouseTOM",
+#                        verbose = 3, deepSplit = T)
+# 
+# # summary of the network modules (colors represent module assignment)
+# net_df = data.frame(net$colors) # convert to a df
+# net_df = cbind(id = rownames(net_df), net_df) # change the index (node names) to a column
+# rownames(net_df) = 1:nrow(net_df)
+# colnames(net_df)[2] = 'louvain_label' # change column name
+# 
+# # change the file name below to wgcna_modules.csv during test
+# # write.csv(net_df, './Data/pipeline/human/network_analysis/wgcna_modules.csv', row.names = F)
+# write.csv(net_df, 'G:/Shared drives/NIAAA_ASSIST/Data/HDID_data/eda_derived/PFC_wgcna_modules.csv', row.names = F)
