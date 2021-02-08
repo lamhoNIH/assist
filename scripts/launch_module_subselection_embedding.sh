cee70e57d4296e873eaa99e8b1723e2cd5ccf819
gzheng2 <gzheng@eagc.test>
2021-02-08 11:12:29 -0500

    reduced memory limit from 40g to 24g

--- scripts/launch_module_subselection_embedding.sh
+++ scripts/launch_module_subselection_embedding.sh
@@ -14,6 +14,7 @@ else
 fi
 
 MODULE="module_subselection_embedding"
+OUTPUT_PATH="pipeline/${DATASET}/${MODULE}"
 
 if [ ! -d "${DATA}/${OUTPUT_PATH}" ] 
 then
@@ -23,6 +24,6 @@ fi
 cp "${SCRIPT_DIR}/${DATASET}/${MODULE}".json "${DATA}/${OUTPUT_PATH}"/config.json
 
 date
-# tried 32G and the process got killed. The process took 8.5 hours on Macbook Pro with 2.4 GHz 8-Core Intel Core i9 and 64 GB 2667 MHz DDR4
-docker run --rm -m 40g -e config_file="Data/${OUTPUT_PATH}/config.json" -e archive_path="Data/${OUTPUT_PATH}" -e run_num="run1" -v "${DATA}":/assist/Data assist/${MODULE}:0.1.0
+# tried -m 24g successfully. The process took 8.5 hours on Macbook Pro with 2.4 GHz 8-Core Intel Core i9 and 64 GB 2667 MHz DDR4
+docker run --rm -m 24g -e config_file="Data/${OUTPUT_PATH}/config.json" -e archive_path="Data/${OUTPUT_PATH}" -e run_num="run2" -v "${DATA}":/assist/Data assist/${MODULE}:0.1.0
 date
