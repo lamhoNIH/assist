555a1b28bf29dbfa6182e0d8f103a3dab02e7427
gzheng2 <gzheng@eagc.test>
2021-02-08 11:12:49 -0500

    updated run number

--- scripts/launch_module_extraction.sh
+++ scripts/launch_module_extraction.sh
@@ -26,5 +26,5 @@ cp "${SCRIPT_DIR}/${DATASET}/${MODULE}".json "${DATA}/${OUTPUT_PATH}"/config.jso
 # Takes about 8 minutes to run on human data
 date
 # tried -m 28g, Docker host (28GB, Swap 4GB, Disk image size 160GB)
-docker run -m 28g --rm -e config_file="Data/${OUTPUT_PATH}/config.json" -e archive_path="Data/${OUTPUT_PATH}/test" -v "${DATA}":/assist/Data assist/${MODULE}:0.1.0
+docker run -m 28g --rm -e config_file="Data/${OUTPUT_PATH}/config.json" -e archive_path="Data/${OUTPUT_PATH}/run2" -v "${DATA}":/assist/Data assist/${MODULE}:0.1.0
 date
