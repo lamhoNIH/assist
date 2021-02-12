1ee3e84eacceabdedd8c90fbf388a2c72d596727
gzheng2 <gzheng@eagc.test>
2021-02-12 10:33:39 -0500

    updated run number

--- scripts/launch_module_subselection_embedding.py
+++ scripts/launch_module_subselection_embedding.py
@@ -30,5 +30,5 @@ if not exists(f"{data}/{output_path}"):
 copyfile(f"{script_dir}/{dataset}/{module}.json", f"{data}/{output_path}/config.json")
 
 print(f"{datetime.now()}")
-system(f'docker run --rm -m 24g -e config_file="Data/{output_path}/config.json" -e archive_path="Data/{output_path}" -e run_num="run2" -v "{data}":/assist/Data assist/{module}:0.1.0')
+system(f'docker run --rm -m 24g -e config_file="Data/{output_path}/config.json" -e archive_path="Data/{output_path}" -e run_num="run3" -v "{data}":/assist/Data assist/{module}:0.1.0')
 print(f"{datetime.now()}")
