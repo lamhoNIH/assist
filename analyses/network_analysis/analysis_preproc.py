798a8ba50b918404840d3b232ae3012d2c233c51
gzheng2 <gzheng@eagc.test>
2021-04-06 13:48:10 -0400

    handle string representation of boolean

--- analyses/network_analysis/analysis_preproc.py
+++ analyses/network_analysis/analysis_preproc.py
@@ -2,27 +2,23 @@ import argparse
 import json
 import os
 import pandas as pd
-from preproc.input import Input
-from preproc.result import Result
+#from preproc.input import Input
+#from preproc.result import Result
 
-def preproc(config_file, archive_path):
-    Input('./Data')
-    data_folder = Input.getPath()
-    print("config_file: {} data_folder: {} archive_path: {}".format(config_file, data_folder, archive_path))
-    Result(archive_path, overwrite=False)
-
+def preproc(config_file):
+    print(f'inside preproc: {config_file}')
     with open(config_file) as json_data:
         config_json = json.load(json_data)
-    if ("skip_preproc" not in config_json["parameters"]) or (config_json["parameters"]["skip_preproc"] is False):
-        meta = pd.read_csv(os.path.join(data_folder, config_json["inputs"]["diagnostics"]))
-        expression = pd.read_csv(os.path.join(data_folder, config_json["inputs"]["normalized_counts"]), sep = '\t', index_col = 0)
+    if ("skip_preproc" not in config_json["parameters"]) or (json.loads(config_json["parameters"]["skip_preproc"].lower()) is False):
+        meta = pd.read_csv(config_json["inputs"]["diagnostics"])
+        expression = pd.read_csv(config_json["inputs"]["normalized_counts"], sep = '\t', index_col = 0)
         expression_meta = pd.merge(expression.T, meta, left_index = True, right_on = 'IID')
-        expression_meta.to_csv(os.path.join(Result.getPath(), config_json["outputs"]["expression_with_metadata"]), index = 0)
+        expression_meta.to_csv(os.path.join(config_json["outputs"]["expression_with_metadata"]), index = 0)
     else:
+        print("skip_preproc")
         return None
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_file", help="path to configuration file")
-    parser.add_argument("--archive_path", help="path to save results")
     args = parser.parse_args()
-    preproc(args.config_file, args.archive_path)
+    preproc(args.config_file)
