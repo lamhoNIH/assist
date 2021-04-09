e50ed3d01c4607bb28907e4f7351b0a501a18602
gzheng2 <gzheng@eagc.test>
2021-04-08 20:10:38 -0400

    removed archive_path, changed computed_networks to gene_to_module_mapping, and changed paths to absolute paths

--- analyses/module_membership_analysis/membership.py
+++ analyses/module_membership_analysis/membership.py
@@ -4,32 +4,28 @@ import os
 import shutil
 import pandas as pd
 
-from src.preproc.input import Input
-from src.preproc.result import Result
+from preproc.input import Input
+from preproc.result import Result
+from eda.eda_functions import *
 
-Input('./Data')
-
-from src.eda.eda_functions import *
-
-def analyze_membership(config_file, archive_path):
-    data_folder = Input.getPath()
-    print("config_file: {} data_folder: {} archive_path: {}".format(config_file, data_folder, archive_path))
-    Result(archive_path)
-    shutil.copy(config_file, Result.getPath())
+def analyze_membership(config_file):
+    print("config_file: {}".format(config_file))
 
     with open(config_file) as json_data:
         config_json = json.load(json_data)
 
-    computed_networks_df = pd.read_csv(os.path.join(data_folder, config_json["computed_networks"]))
-    comm_df1 = pd.read_csv(os.path.join(data_folder, config_json["network_louvain_default"]))
-    comm_df2 = pd.read_csv(os.path.join(data_folder, config_json["network_louvain_agg1"]))
+    Result(config_json["parameters"]["plot_path"])
+
+    computed_networks_df = pd.read_csv(config_json["inputs"]["gene_to_module_mapping"])
+    comm_df1 = pd.read_csv(config_json["inputs"]["network_louvain_default"])
+    comm_df2 = pd.read_csv(config_json["inputs"]["network_louvain_agg1"])
 
     comm_dfs = [computed_networks_df, comm_df1, comm_df2]
     comm_names = ['wgcna', 'louvain 1', 'louvain 2']
     plot_gene_cnt_each_cluster(comm_dfs, 'louvain_label', comm_names)
     
-    if "skip_network_cluster_stability" not in config_json or config_json["skip_network_cluster_stability"] is False:
-        expression_meta_df = pd.read_csv(os.path.join(data_folder, config_json["expression_with_metadata"]), low_memory = False)
+    if ("skip_network_cluster_stability" not in config_json["parameters"]) or (json.loads(config_json["parameters"]["skip_network_cluster_stability"].lower()) is False):
+        expression_meta_df = pd.read_csv(config_json["inputs"]["expression_with_metadata"], low_memory = False)
         cluster_pair_wgcna_n_com1, network_cluster_stability1 = network_cluster_stability(computed_networks_df, comm_df1, 'louvain_label', expression_meta_df)
         for cluster in comm_df1.louvain_label.unique():
             plot_random_vs_actual_z(computed_networks_df, comm_df1, cluster_pair_wgcna_n_com1[cluster], cluster, 'louvain_label', network_cluster_stability1, 'wgcna vs louvain 1', expression_meta_df)
@@ -37,7 +33,6 @@ def analyze_membership(config_file, archive_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_file", help="path to configuration file")
-    parser.add_argument("--archive_path", help="path to save results")
     args = parser.parse_args()
     
-    analyze_membership(args.config_file, args.archive_path)
+    analyze_membership(args.config_file)
