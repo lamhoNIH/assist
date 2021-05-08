0e15324a334363f3aab4e7252e066704c20114d0
Yi-Pei Chen <52116103+bbchen33@users.noreply.github.com>
2021-05-07 18:08:08 -0700

    updated notebooks and functions

--- src/eda/eda_functions_extra.py
+++ src/eda/eda_functions_extra.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.append('../../src')
 import os
+import netcomp
+import pandas as pd
 import math
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
 from sklearn.cluster import KMeans
 from sklearn.metrics import normalized_mutual_info_score as nmi
 from sklearn.decomposition import PCA
@@ -79,6 +82,68 @@ def plot_gene_cnt_each_cluster_v2(cluster_df, cluster_column, network_name, name
     plt.title(network_name)
     plt.tight_layout()
     plt.savefig(os.path.join(Result.getPath(), f"plot_gene_cnt_each_cluster{name_spec}.png"))
+    plt.show()
+    plt.close()
+
+def get_graph_distance(wholenetwork_np, network_np):
+    dc_distance = netcomp.deltacon0(wholenetwork_np, network_np)
+    ged_distance = netcomp.edit_distance(wholenetwork_np, network_np)
+    return dc_distance, ged_distance
+
+def plot_graph_distances(dc_distance_list, ged_distance_list, names):
+    width = (len(dc_distance_list)+1)*2
+    plt.figure(figsize=(width, 5))
+    plt.rcParams.update({'font.size': 18})
+    plt.subplot(1, 2, 1)
+    plt.bar(names, dc_distance_list)
+    plt.title('Deltacon distance')
+    plt.xticks(rotation = 45, ha = 'right')
+
+    plt.subplot(1, 2, 2)
+    plt.bar(names, ged_distance_list)
+    plt.title('GEM distance')
+    plt.xlabel('Number of edges')
+    plt.xticks(rotation = 45, ha = 'right')
+    plt.subplots_adjust(wspace=0.5)
+    plt.tight_layout()
+    plt.savefig(os.path.join(Result.getPath(), "plot_graph_distance.png"))
+    plt.show()
+    plt.close()
+
+def plot_graph_distance(networks, network_names):
+
+    dc_distance_list = []
+    ged_distance_list = []
+    names = []
+    network1 = networks[0]
+    ## compare all the network starting from the second to the first network (reduce run time)
+    i = 1
+    for network in networks[1:]:
+        dc_distance_list.append(netcomp.deltacon0(network1.values, network.values))
+        ged_distance_list.append(netcomp.edit_distance(network1.values, network.values))
+        names.append(f'{network_names[0]} vs {network_names[i]}')
+        i += 1
+    ## pairwise combination (long run time)
+    # for sub_network1, sub_network2 in combinations(zip(networks, network_names), 2):
+    #     dc_distance_list.append(netcomp.deltacon0(sub_network1[0].values, sub_network2[0].values))
+    #     ged_distance_list.append(netcomp.edit_distance(sub_network1[0].values, sub_network2[0].values))
+    #     names.append(f'{sub_network1[1]} vs {sub_network2[1]}')
+    width = len(networks)*2
+    plt.figure(figsize=(width, 5))
+    plt.rcParams.update({'font.size': 18})
+    plt.subplot(1, 2, 1)
+    plt.bar(names, dc_distance_list)
+    plt.title('Deltacon distance')
+    plt.xticks(rotation = 45, ha = 'right')
+
+    plt.subplot(1, 2, 2)
+    plt.bar(names, ged_distance_list)
+    plt.title('GEM distance')
+    plt.xlabel('Number of edges')
+    plt.xticks(rotation = 45, ha = 'right')
+    plt.subplots_adjust(wspace=0.5)
+    plt.tight_layout()
+    plt.savefig(os.path.join(Result.getPath(), "plot_graph_distance.png"))
     plt.show()
     plt.close()
 
