83c46ae952ea435c4d5e365ca7acafa3fd8c7f15
gzheng2 <gzheng@eagc.test>
2021-06-24 16:12:09 -0400

    construct config based on presence of prop_skip_diagnostics

--- analyses/module_de_diagnostic_correlation/ade.py
+++ analyses/module_de_diagnostic_correlation/ade.py
@@ -11,6 +11,7 @@ def ade_entrypoint_v1(
     in_diagnostics, in_normalized_counts, in_gene_to_module_mapping, in_network_louvain_default, in_network_louvain_agg1, in_differentially_expressed_genes,
     out_expression_with_metadata,
     prop_plot_path,
+    prop_skip_diagnostics,
     prop_docker_mem='10737418240',
     prop_docker_cpu='4', 
     prop_docker_volume_1='../..:/assist/data'
@@ -19,24 +20,45 @@ def ade_entrypoint_v1(
 
     config_path = path.join(work_path, 'module_de_diagnostic_correlation.json')
 
-    # Generate and write out JSON
-    # CONFIG.JSON EXAMPLE: G:\Shared drives\NIAAA_ASSIST\Data\pipeline\human\network_analysis\config.json
-    config = {
-        'inputs': {
-            'differentially_expressed_genes': in_differentially_expressed_genes,
-            'diagnostics': in_diagnostics,
-            'normalized_counts': in_normalized_counts,
-            'gene_to_module_mapping': in_gene_to_module_mapping,
-            'network_louvain_default': in_network_louvain_default,
-            'network_louvain_agg1': in_network_louvain_agg1
-        },
-        'outputs': {
-            'expression_with_metadata': out_expression_with_metadata
-        },
-        'parameters': {
-            'plot_path': prop_plot_path
+    if not prop_skip_diagnostics:
+        # Convert commas to tabs so the script can process it properly
+        new_in_normalized_counts = path.join(work_path, 'in_normalized_counts.tsv')
+        with open(in_normalized_counts, 'r') as f1, open(new_in_normalized_counts, 'w') as f2:
+            reader = csv.reader(f1, delimiter=',')
+            writer = csv.writer(f2, delimiter='\t')
+            writer.writerows(reader)
+        
+        # Generate and write out JSON
+        # CONFIG.JSON EXAMPLE: G:\Shared drives\NIAAA_ASSIST\Data\pipeline\human\network_analysis\config.json
+        config = {
+            'inputs': {
+                'differentially_expressed_genes': in_differentially_expressed_genes,
+                'diagnostics': in_diagnostics,
+                'normalized_counts': new_in_normalized_counts,
+                'gene_to_module_mapping': in_gene_to_module_mapping,
+                'network_louvain_default': in_network_louvain_default,
+                'network_louvain_agg1': in_network_louvain_agg1
+            },
+            'outputs': {
+                'expression_with_metadata': out_expression_with_metadata
+            },
+            'parameters': {
+                'plot_path': prop_plot_path
+            }
         }
-    }
+    else:
+        config = {
+            'inputs': {
+                'differentially_expressed_genes': in_differentially_expressed_genes,
+                'gene_to_module_mapping': in_gene_to_module_mapping,
+                'network_louvain_default': in_network_louvain_default,
+                'network_louvain_agg1': in_network_louvain_agg1
+            },
+            'parameters': {
+                'plot_path': prop_plot_path,
+                'skip_diagnostics': prop_skip_diagnostics
+            }
+        }
 
     with open(config_path, 'w') as f:
         json.dump(config, f, indent=2)
